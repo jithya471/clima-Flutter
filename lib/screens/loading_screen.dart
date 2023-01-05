aa99568da89ce2b8f99853b8e0988af5361c049f
jithya471 <jithya471@gmail.com>
2023-01-05 20:44:38 +0530

    main

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors, library_private_types_in_public_api
+// ignore_for_file: use_key_in_widget_constructors, library_private_types_in_public_api, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
